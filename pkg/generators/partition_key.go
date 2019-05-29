b14fb8a3c06a746daf43c1ace092622b63918536
John Sanda <john.sanda@gmail.com>
2019-05-28 23:41:00 -0400

    initial commit for sequence and random partition key generators

---
+++ pkg/generators/partition_key.go
@@ -0,0 +1,31 @@
+package generators
+
+import (
+	"math/rand"
+	"time"
+)
+
+type PartitionKeyGenerator struct {
+	GenFunc func(uint64) uint64
+	Prefix string
+}
+
+func Sequence(prefix string) PartitionKeyGenerator {
+	current := uint64(0)
+	genFunc := func(max uint64) uint64 {
+		if current > max {
+			current = 0
+		}
+		current++
+		return current
+	}
+	return PartitionKeyGenerator{GenFunc:genFunc, Prefix:prefix}
+}
+
+func Random(prefix string) PartitionKeyGenerator {
+	seededRand := rand.New(rand.NewSource(time.Now().UnixNano()))
+	genFunc := func(max uint64) uint64 {
+		return uint64(seededRand.Int63n(int64(max)))
+	}
+	return PartitionKeyGenerator{GenFunc:genFunc, Prefix:prefix}
+}
